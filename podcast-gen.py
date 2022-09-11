b2cc4a58078336f1c7373363768bce58da588cc5
Miki Vanoušek <vanousekmikulas@gmail.com>
2022-09-11 17:58:57 +0200

    add profile pic

--- podcast-gen.py
+++ podcast-gen.py
@@ -1,34 +1,40 @@
-import feedgen.feed as fg, yaml, os, re, shutil
+import feedgen.feed as fg, yaml, re, shutil, os
+from os.path import join
+
 
 def main():
     podcast_properties = read_yaml_file('podcast.yaml')
-    # Deletes everything in output directory, so that we have a clean build.
-    for filename in os.listdir(podcast_properties['output_root']):
-        file_path = os.path.join(podcast_properties['output_root'], filename)
-    try:
-        if os.path.isfile(file_path) or os.path.islink(file_path):
-            os.unlink(file_path)
-        elif os.path.isdir(file_path):
-            shutil.rmtree(file_path)
-    except Exception as e:
-        print('Failed to delete %s. Reason: %s' % (file_path, e))
+    content_root = podcast_properties['content_root'];
+    episodes_root = join(content_root, podcast_properties['episodes_directory']);
+    logo = podcast_properties['logo']
+
+    output_root = podcast_properties['output_root']
+    url_base = podcast_properties['url_base']
+
+    delete_contents(output_root)    
 
     generator = fg.FeedGenerator()
     generator.load_extension('podcast')
     call_setters(generator, podcast_properties['rss'])
-    generator.id(f'{podcast_properties["url_base"]}/atom.xml')
+    generator.id(f'{url_base}/atom.xml')
     generator.link(href='https://www.vanousek.com', rel='alternate')
-    generator.link(href=f'{podcast_properties["url_base"]}/rss.xml')
+    generator.link(href=f'{url_base}/rss.xml', rel='self')
 
-    if not os.path.exists(podcast_properties['output_root']):
+    if not os.path.exists(output_root):
         # TODO Create output dir instead, ensure permissions
         raise Exception('The output root directory needs to exist!')
 
-    parse_episodes(podcast_properties['episodes_root'], generator,
-        podcast_properties['output_root'], podcast_properties['url_base'])
+    parse_episodes(episodes_root, generator, output_root, url_base)
+    
+    logo_path = join(content_root, logo)
+    if os.path.exists(logo_path):
+        os.symlink(logo_path, join(output_root, logo))
+        generator.logo(f'{url_base}/{logo}')
+    else:
+        print(f'WARNING: No logo found at {logo_path}')
 
-    generator.rss_file(os.path.join(podcast_properties['output_root'], 'rss.xml'), pretty=True) # Write the RSS feed to a file
-
+    # Write the RSS feed to a file
+    generator.rss_file(join(output_root, 'rss.xml'), pretty=True)
     # generator.atom_file('atom.xml', pretty=True) # Write the ATOM feed to a file
 
 def read_yaml_file(location):
@@ -45,7 +51,8 @@ def call_setters(object, properties):
         setter = getattr(object, key)
         setter(value)
 
-def parse_episodes(episodes_root, feed_generator, output_root, url_base):
+# TODO Ep pic
+def parse_episodes(episodes_root,feed_generator, output_root, url_base):
     regex = '\d{4}([-]\w*)+'
     pattern = re.compile(regex)
 
@@ -54,11 +61,11 @@ def parse_episodes(episodes_root, feed_generator, output_root, url_base):
             print(f'WARNING: Skipping {episode_dir_name}, it does not match the {regex}'
                 + ' regular expression pattern')
             continue
-        episode_sound_location = os.path.join(episodes_root, episode_dir_name, 'ep.mp3')
+        episode_sound_location = join(episodes_root, episode_dir_name, 'ep.mp3')
         if not os.path.exists(episode_sound_location):
             print(f'WARNING: Skipping {episode_dir_name}, it does not contain ep.mp3')
             continue
-        episode_properties_location = os.path.join(episodes_root, episode_dir_name, 'ep.yaml')
+        episode_properties_location = join(episodes_root, episode_dir_name, 'ep.yaml')
         if not os.path.exists(episode_properties_location):
             print(f'WARNING: Skipping {episode_dir_name}, it does not contain ep.yaml')
             continue
@@ -75,9 +82,21 @@ def parse_episodes(episodes_root, feed_generator, output_root, url_base):
         fe.description(episode_properties['description'])
         fe.enclosure(episode_url, 0, 'audio/mpeg')
 
-        destination_directory = os.path.join(output_root, 'eps', name_components[0])
+        destination_directory = join(output_root, 'eps', name_components[0])
         os.makedirs(destination_directory)
-        os.symlink(episode_sound_location, os.path.join(destination_directory, 'ep.mp3')) 
+        os.symlink(episode_sound_location, join(destination_directory, 'ep.mp3')) 
 
+def delete_contents(directory_path):
+    print(f'Deleting {directory_path}:') 
+    for filename in os.listdir(directory_path):
+        file_path = join(directory_path, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
+            
 if __name__ == '__main__':
     main();
